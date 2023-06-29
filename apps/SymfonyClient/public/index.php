fb1d6e1f04d5f5ccc59757d66feb86cd96c8e83d
Ramon Serra <ramon.serra@real-digital.de>
2023-06-29 15:12:51 +0200

    Install and configure a decoupled Symfony

--- apps/SymfonyClient/public/index.php
+++ apps/SymfonyClient/public/index.php
@@ -1,2 +1,9 @@
 <?php
-phpinfo();
+
+use SymfonyClient\Kernel;
+
+require_once dirname(__DIR__).'/../../vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
